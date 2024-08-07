19ee8fc94f1205604526e6ccd2a64793097b83c6
Kumushbekov <b7030n@gmail.com>
2024-08-07 19:55:32 +0200

    Kinopoisk beginning

---
+++ main/script.js
@@ -0,0 +1,9 @@
+fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films', {
+    method: 'GET',
+    headers: {
+        'X-API-KEY': '4c82cfd7-6c36-4b4a-ab2a-21eb945ff301',
+        'Content-Type': 'application/json',
+    },
+})
+
+
